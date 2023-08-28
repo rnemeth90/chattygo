77e7b3bbe45ffe7faf8cf028cf0c9de3b7cae6c7
ryan nemeth <ryannemeth@live.com>
2023-08-28 16:08:00 -0400

    working version

--- cmd/chattygo/main.go
+++ cmd/chattygo/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
 
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func getGPT3Response(prompt string) string {
+	client := openai.NewClient(os.Getenv("OPENAI_API_KEY")) // Replace with your OpenAI API Key
+
+	completion, err := client.CreateChatCompletion(context.TODO(), openai.ChatCompletionRequest{
+		Model: openai.GPT3Dot5Turbo,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    "user",
+				Content: prompt,
+			},
+		},
+	})
+
+	if err != nil {
+		fmt.Println("Error getting response:", err)
+		return "Error"
+	}
+
+	return completion.Choices[0].Message.Content
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("You: ")
+		text, _ := reader.ReadString('\n')
+		text = text[:len(text)-1] // remove newline character
+
+		if text == "exit" {
+			break
+		}
+
+		response := getGPT3Response(text)
+		fmt.Println("GPT-3:", response)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rnemeth90/chattygo
 
 go 1.20
+
+require github.com/sashabaranov/go-openai v1.14.2 // indirect
